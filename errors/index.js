7ef9c9132e409255f05c4285c4920889155167bd
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2023-01-17 15:03:35 +0700

    fix error/index

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,4 @@
-import CustomAPIError from "./custom-api.js";
+import CustomAPIError from "./Custom-api.js";
 import BadRequestError from "./BadRequestError.js";
 import NotFoundError from "./NotFoundError.js";
 import UnauthenticatedError from "./unauthenticated.js";
